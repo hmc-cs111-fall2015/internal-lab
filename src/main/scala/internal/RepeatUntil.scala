298e4a03a6707288293eaadcb1dbd8ce0c408629
Dan Obermiller <dannnno16@gmail.com>
2015-09-23 14:15:31 -0700

    Implement control flow structures
    Internal DSLs lab

--- src/main/scala/internal/RepeatUntil.scala
+++ src/main/scala/internal/RepeatUntil.scala
@@ -12,6 +12,14 @@ package internal
 object RepeatUntil extends App {
   
   // define the new control-flow structure here
+  
+  implicit class repeat(body: =>Any) {
+    def until(condition: =>Boolean):Unit = {
+      while (!condition) {
+        body
+      }
+    }
+  }
 
   var i = 0
   repeat  {
