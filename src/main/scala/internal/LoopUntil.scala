061986adad3cebb1030ec4a584a952864244f259
Alex Ozdemir <aozdemir@hmc.edu>
2015-09-23 14:18:19 -0700

    Did first two

--- src/main/scala/internal/LoopUntil.scala
+++ src/main/scala/internal/LoopUntil.scala
@@ -10,10 +10,15 @@ package internal
  */
 object LoopUntil extends App {
 
+  def loop_until(cond: =>Boolean)(code: =>Unit) = {
+    while (!cond) {
+      code
+    }
+  }
   // define the new control-flow structure here
 
   var i = 0
-  
+
   loop_until (i > 9) {
       if ( (i % 2) == 0 )
           println(i)
